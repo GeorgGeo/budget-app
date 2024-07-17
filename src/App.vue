3d894a240c27e9ef4d9bee216de0b4190f6b4d97
Georggeo <georg314413@gmail.com>
2024-07-17 14:17:39 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="price-block">
+      <p>Original Price: {{ originalPrice }}</p>
+      <input type="number" v-model="discount">
+      <p>Discount: {{ discount }}</p>
+      <p>Finally Price: {{ finallyPrice }}</p>
+    </div>
+
+    <MethodsVue />
+
+    <!-- Обчисліть загальну вартість телефонів студентів вашого класу, використавши метод computed. При бажанні можете вивести дані на екран. -->
+     <div class="price-student">
+      <p>Total Price: {{ totalPrice }}</p>
+     </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref, computed } from 'vue';
+import MethodsVue from './components/MethodsVue.vue';
 
-export default {
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    MethodsVue,
+  },
+  setup() {
+
+    const originalPrice = ref(100);
+    const discount = ref(null);
+
+    console.log(originalPrice);
+
+    const finallyPrice = computed(() => {
+      return originalPrice.value - discount.value;
+    });
+    // Homework
+    const studentList = ref([
+      {
+        name: 'Yuriy',
+        age: 29,
+        phone: 'Apple',
+        price: 2900,
+      },
+      {
+        name: 'Vika',
+        age: 39,
+        phone: 'Samsung',
+        price: 1900,
+      },
+      {
+        name: 'Vova',
+        age: 25,
+        phone: 'Apple',
+        price: 2000,
+      },
+      {
+        name: 'Vita',
+        age: 29,
+        phone: 'Apple',
+        price: 3000,
+      },
+    ]);
+
+    const totalPrice = computed(() => {
+      return studentList.value.reduce((acc, item) => acc += item.price, 0)
+    });
+
+    return {
+      originalPrice,
+      discount,
+      finallyPrice,
+      // Homework
+      studentList,
+      totalPrice
+    }
   }
-}
+})
 </script>
 
 <style>
---
+++ src/components/homeWork.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Обчисліть загальну вартість телефонів студентів вашого класу, використавши метод computed. При бажанні можете вивести дані на екран. -->
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  setup() {
+
+    return {
+
+    }
+
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
